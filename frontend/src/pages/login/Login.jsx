82f3f25a0f7f0c6e5cb263f8f08e808e9be6ffa5
Amith Wijesinghe <amithmwijesinghe@gmail.com>
2022-06-27 23:43:32 +0530

    frontend pages and stylesheets

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import "./Login.css";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { user, loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      navigate("/products");
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
+
+  console.log(user);
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="lInput"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="lInput"
+          onChange={handleChange}
+        />
+        <button className="lButton" onClick={handleClick}>
+          Login
+        </button>
+        {error && <span>{error.message}</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/pages/products/Products.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import "./Products.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { useEffect } from "react";
+import axios from "axios";
+
+const Products = () => {
+  const [list, setList] = useState([]);
+  const { data, loading, error } = useFetch("/products");
+
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/products/${id}`);
+    } catch (err) {}
+    setList(list.filter((listItem) => listItem.id !== id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "productName", headerName: "Product Name", width: 130 },
+    { field: "desc", headerName: "Description", width: 130 },
+    {
+      field: "size",
+      headerName: "Size",
+      width: 60,
+    },
+    {
+      field: "color",
+      headerName: "Color",
+      width: 90,
+    },
+    {
+      field: "qty",
+      headerName: "Quantity",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      type: "number",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/${10}`}>
+              <button className="productEdit">Edit</button>
+            </Link>
+            <button
+              className="productDelete"
+              onClick={handleDelete(params.row._id)}
+            >
+              Delete
+            </button>
+          </>
+        );
+      },
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      productName: "Snow",
+      desc: "Jon",
+      size: 35,
+      color: "Green",
+      qty: 10,
+      price: 150,
+    },
+  ];
+
+  return (
+    <div>
+      <div style={{ height: "80vh", width: "100%" }}>
+        <DataGrid
+          rows={list}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+          disableSelectionOnClick
+          getRowId={(row) => row._id}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
