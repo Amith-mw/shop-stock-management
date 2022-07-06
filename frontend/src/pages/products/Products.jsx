b40bc307521ebc95e5e64a5eec896dd98fc96c1f
Amith Wijesinghe <amithmwijesinghe@gmail.com>
2022-07-06 10:13:37 +0530

    modified - products page

--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -1,104 +1,104 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Products.css";
 import { DataGrid } from "@mui/x-data-grid";
-import { Link } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
-import { useEffect } from "react";
-import axios from "axios";
 import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { products } from "../../../src/dataSet/dummyData";
+import Button from "../../components/button/Button";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+
+const columns = [
+  // { field: "id", headerName: "ID", width: 70 },
+  { field: "productName", headerName: "Title", width: 150 },
+  { field: "brandId", headerName: "Brand", width: 150 },
+  { field: "categoryId", headerName: "Category", width: 150 },
+  { field: "subCategoryId", headerName: "Sub Category", width: 150 },
+  { field: "size", headerName: "Size", width: 60 },
+  { field: "color", headerName: "Color", width: 60 },
+  {
+    field: "price",
+    headerName: "Price",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "quantity",
+    headerName: "Quantity",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "action",
+    headerName: "Actions",
+    width: 250,
+    renderCell: (params) => {
+      return (
+        <>
+          {/* <Button
+            btnName={""}
+            btnColor={"blue"}
+            btnIcon={<AddOutlinedIcon />}
+            style={{ marginRight: "0.5rem" }}
+          /> */}
+          <Button
+            btnName={""}
+            btnColor={"green"}
+            btnIcon={<ModeEditOutlineOutlinedIcon />}
+            style={{ marginRight: "0.5rem" }}
+          />
+          <Button
+            btnName={""}
+            btnColor={"red"}
+            btnIcon={<DeleteOutlinedIcon />}
+          >
+            <DeleteOutlinedIcon />
+          </Button>
+        </>
+      );
+    },
+  },
+
+  // {
+  //   field: "fullName",
+  //   headerName: "Full name",
+  //   description: "This column has a value getter and is not sortable.",
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params) =>
+  //     `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  // },
+];
+
+const productList = products.map((product) => product);
+const actionButtons = [];
 
 const Products = () => {
-  const [list, setList] = useState([]);
-  const { data, loading, error } = useFetch("/products");
-
-  useEffect(() => {
-    setList(data);
-  }, [data]);
-
-  const handleDelete = async (id) => {
-    try {
-      await axios.delete(`/products/${id}`);
-    } catch (err) {}
-    setList(list.filter((listItem) => listItem.id !== id));
-  };
-
-  const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "productName", headerName: "Product Name", width: 130 },
-    { field: "desc", headerName: "Description", width: 130 },
-    {
-      field: "size",
-      headerName: "Size",
-      width: 60,
-    },
-    {
-      field: "color",
-      headerName: "Color",
-      width: 90,
-    },
-    {
-      field: "qty",
-      headerName: "Quantity",
-      type: "number",
-      width: 90,
-    },
-    {
-      field: "price",
-      headerName: "Price",
-      type: "number",
-      width: 90,
-    },
-    {
-      field: "action",
-      headerName: "Action",
-      type: "number",
-      width: 150,
-      renderCell: (params) => {
-        return (
-          <>
-            <Link to={`/user/${10}`}>
-              <button className="productEdit">Edit</button>
-            </Link>
-            <button
-              className="productDelete"
-              onClick={handleDelete(params.row._id)}
-            >
-              Delete
-            </button>
-          </>
-        );
-      },
-    },
-  ];
-
-  const rows = [
-    {
-      id: 1,
-      productName: "Snow",
-      desc: "Jon",
-      size: 35,
-      color: "Green",
-      qty: 10,
-      price: 150,
-    },
-  ];
-
   return (
-    <div className="ordersContainer">
+    <div className="products">
       <Topbar />
       <div className="pageContentContainer">
         <Sidebar />
-        <div className="dataTableContainer">
-          <h2 className="orderTitle">Products</h2>
-          <div style={{ height: "80vh", width: "100%" }}>
+        <div className="productsContent">
+          <div className="productTitleRow">
+            <h2 className="productsTitle">Products</h2>
+            <Button
+              btnName={""}
+              btnColor={"blue"}
+              btnIcon={<AddOutlinedIcon />}
+              style={{ marginRight: "0.5rem" }}
+            />
+          </div>
+
+          <div className="productsTable">
             <DataGrid
-              rows={list}
+              rows={productList}
               columns={columns}
-              pageSize={10}
-              rowsPerPageOptions={[10]}
+              pageSize={15}
+              rowsPerPageOptions={[5]}
               disableSelectionOnClick
-              getRowId={(row) => row._id}
+              //   checkboxSelection
             />
           </div>
         </div>
