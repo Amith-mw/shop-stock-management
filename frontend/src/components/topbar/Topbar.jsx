29ad783b50145b8332ea697415d37a5cbe05f211
Amith Wijesinghe <amithmwijesinghe@gmail.com>
2022-07-06 10:04:30 +0530

    added - top navbar component

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -6,10 +6,16 @@ function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <h4 className="topUserType">Admin</h4>
+          <span className="logo">ShopIt</span>
         </div>
         <div className="topRight">
           <h3>INVENTORY MANAGEMENT SYSTEM</h3>
+          {/* TODO: Icons Area */}
+          {/* 
+          01. notifications for minimum product quantity 
+          02. profile pic - when click on it - show user role and logout
+          */}
+          {/* <div className="topbarIcons"><NotificationsNoneIcon/></div> */}
         </div>
       </div>
     </div>
